6d0fd9957fe4e3f5d3053b3f99460df6d733fb24
teddyconstant <teddyconstant-lagaville@hotmail.fr>
2017-02-13 14:41:41 +0100

    Ajout du fichier entete.php

---
+++ entete.php
@@ -0,0 +1,23 @@
+<header><!-- En-tête de la page -->
+		
+			<h1 class="titre_1">les créations tcl</h1>
+			<h2 class="titre_2">infographie, développeur web, customize vos murs d'intérieur et objets, avec du vinyle</h2>
+			<p class="bienvenu">Bonjour, soyez les bienvenu sur mon site : <strong> Les Créations TCL </strong>, cliquez sur les liens ci dessous, afin d'accéder aux autres pages du site.</p>
+			
+		
+		
+			<!-- Menu navigation -->
+			<nav>
+				<ul>
+					<li><a href="index.html">accueil</a></li>
+					<li><a href="web.html">web</a></li>
+					<li><a href="infographie.html">infographie</a></li>
+					<li><a href="contact.html">me contacter</a></li>
+				</ul>
+			</nav>
+			<!-- Fin du Menu navigation -->
+			
+			<form method="post" action="traitement.php><label for="recherche></label><input type="text" name="recherche" id="recherche" placeholder="Recherche"size="30" maxlength="10"/>
+			</form>
+				
+</header><!-- Fin En-tête de la page -->
